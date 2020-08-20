66dd3515c022449878dab4c65f53e990b5122e5e
NekoFuwa1739 <62869023+NekoFuwa1739@users.noreply.github.com>
2020-08-20 09:24:52 +0900

    Add files via upload

---
+++ dc.py
@@ -0,0 +1,44 @@
+import discord
+import requests
+
+discord_token = "https://discord.com/api/webhooks/743861310390927430/7dmHAC_kw--P3-3Jcz9B5MmeW8uFr0rxG0VOMch9yVsa6S-5SD2qBldKLminJpUYq0cU"#とーくん
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Done')
+
+@client.event
+async def on_message(message):
+
+    if message.author.bot:
+        return
+
+    if client.user in message.mentions:
+        url = 's.mclife.pro:51019'#じぶんのさばのipに変えてね
+        response = requests.get(url)
+        jsonData = response.json()
+
+        if jsonData["online"]==False:
+            embed = discord.Embed(title="さばの状態", description="オフライン", color=0xff0000)
+            await message.channel.send(embed=embed)
+            return
+        plyrs=""
+        
+        try:
+            for i in jsonData["players"]["list"]:
+                plyrs=plyrs+i+"\n"
+        except KeyError:
+            plyrs="プレイヤーはいません。\n" 
+            
+        embed = discord.Embed(title="さばめい", description="オンライン", color=0x00ff00, url="example.com")
+        embed.set_author(name="たいとる", url="example.com", icon_url="example.com/sample.png")
+        embed.add_field(name="IP:PORT", value=jsonData['hostname']+":"+str(jsonData['port']))
+        embed.add_field(name="人数", value=str(jsonData['players']['online'])+"/"+str(jsonData["players"]["max"]))
+        embed.add_field(name="サーバー内にいる人", value=plyrs, inline=False)
+        await message.channel.send(embed=embed)
+        return
+
+
+client.run(discord_token)
